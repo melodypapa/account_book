1e70baaf21746f4ffe4a6fd20dd06988836611e1
melodypapa <melodypapa@outlook.com>
2020-01-01 21:32:01 +0800

    add the validation for each text input

--- lib/pages/deposit/deposit_page.dart
+++ lib/pages/deposit/deposit_page.dart
@@ -46,92 +46,115 @@ class _DepositEditFormState extends State<DepositEditForm> {
     Provider.of<DepositService>(context).loadBankOptions();
 
     return SizedBox(
-      child:
-          Consumer<DepositService>(builder: (context, depositService, child) {
-        return DropdownButtonHideUnderline(
-          child: Form(
-            key: _formKey,
-            child: ListView(
-              children: <Widget>[
-                DateTimePicker(
-                  labelText: 'From',
-                  selectedDate: _fromDate,
-                  selectDate: (DateTime date) {
-                    setState(() {
-                      _fromDate = date;
-                    });
-                  },
-                ),
-                const SizedBox(height: 16.0),
-                DepositTextField(
-                  label: "Amount",
-                  currentFocusField: _amountFocusNode,
-                  controller: _amountController,
-                  nextFocusField: _peroidFocusNode,
-                  border: OutlineInputBorder(),
-                ),
-                DepositTextField(
-                  label: "Period (Months)",
-                  currentFocusField: _peroidFocusNode,
-                  nextFocusField: _interestFocusNode,
-                  controller: _periodController,
-                ),
-                DepositTextField(
-                  label: 'Interest(%)',
-                  currentFocusField: _interestFocusNode,
-                  controller: _interestController,
-                  nextFocusField: _bankFocusNode,
-                ),
-                InputDecorator(
-                  decoration: const InputDecoration(
-                    labelText: 'Bank',
-                    hintText: 'Choose the bank',
+      child: Consumer<DepositService>(
+        builder: (context, depositService, child) {
+          return DropdownButtonHideUnderline(
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: <Widget>[
+                  DateTimePicker(
+                    labelText: 'From',
+                    selectedDate: _fromDate,
+                    selectDate: (DateTime date) {
+                      setState(() {
+                        _fromDate = date;
+                      });
+                    },
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Expanded(
-                        child: DropdownButton<int>(
-                          focusNode: _bankFocusNode,
-                          value: depositService.currentBankId,
-                          onChanged: (int bankId) {
-                            depositService.currentBankId = bankId;
-                          },
-                          items: depositService.bankOptions
-                              .map<DropdownMenuItem<int>>((Bank bank) {
-                            return DropdownMenuItem<int>(
-                              value: bank.bankId,
-                              child: Text(bank.name),
-                            );
-                          }).toList(),
+                  const SizedBox(height: 16.0),
+                  DepositTextField(
+                    label: "Amount",
+                    currentFocusField: _amountFocusNode,
+                    controller: _amountController,
+                    nextFocusField: _peroidFocusNode,
+                    border: OutlineInputBorder(),
+                    validation: (String value){
+                      if (value.isEmpty){
+                        return 'Please enter the amount.';
+                      }
+                      return null;
+                    },
+                  ),
+                  DepositTextField(
+                    label: "Period (Months)",
+                    currentFocusField: _peroidFocusNode,
+                    nextFocusField: _interestFocusNode,
+                    controller: _periodController,
+                    validation: (String value){
+                      if (value.isEmpty){
+                        return "Please enter the period.";
+                      }
+                      return null;
+                    },
+                  ),
+                  DepositTextField(
+                    label: 'Interest(%)',
+                    currentFocusField: _interestFocusNode,
+                    controller: _interestController,
+                    nextFocusField: _bankFocusNode,
+                    validation: (String value){
+                      if (value.isEmpty){
+                        return 'Please enter the interest of each month';
+                      }
+                      return null;
+                    },
+                  ),
+                  InputDecorator(
+                    decoration: const InputDecoration(
+                      labelText: 'Bank',
+                      hintText: 'Choose the bank',
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Expanded(
+                          child: DropdownButton<int>(
+                            focusNode: _bankFocusNode,
+                            value: depositService.currentBankId,
+                            onChanged: (int bankId) {
+                              depositService.currentBankId = bankId;
+                            },
+                            items: depositService.bankOptions
+                                .map<DropdownMenuItem<int>>((Bank bank) {
+                              return DropdownMenuItem<int>(
+                                value: bank.bankId,
+                                child: Text(bank.name),
+                              );
+                            }).toList(),
+                          ),
                         ),
-                      ),
-                      RaisedButton(
-                        child: Text("Add Bank"),
-                        onPressed: () async {
-                          depositService.currentBankId =
-                              await Navigator.of(context)
-                                  .push(MaterialPageRoute<int>(
-                            builder: (BuildContext context) => BankFormPage(),
-                            fullscreenDialog: true,
-                          ));
-                        },
-                      ),
-                    ],
+                        RaisedButton(
+                          child: Text("Add Bank"),
+                          onPressed: () async {
+                            depositService.currentBankId =
+                                await Navigator.of(context)
+                                    .push(MaterialPageRoute<int>(
+                              builder: (BuildContext context) => BankFormPage(),
+                              fullscreenDialog: true,
+                            ));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8.0),
-                ListTile(
-                  trailing: RaisedButton(
-                    child: Text("Save"),
-                    onPressed: () {},
-                  ),
-                )
-              ],
+                  const SizedBox(height: 8.0),
+                  ListTile(
+                    trailing: RaisedButton(
+                      child: Text("Save"),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      }),
+          );
+        },
+      ),
     );
   }
 }
@@ -144,6 +167,7 @@ class DepositTextField extends StatelessWidget {
     @required this.currentFocusField,
     this.nextFocusField,
     this.border,
+    @required this.validation,
   }) : super(key: key);
 
   final String label;
@@ -151,6 +175,7 @@ class DepositTextField extends StatelessWidget {
   final FocusNode nextFocusField;
   final TextEditingController controller;
   final InputBorder border;
+  final String Function(String) validation;
 
   @override
   Widget build(BuildContext context) {
@@ -170,6 +195,7 @@ class DepositTextField extends StatelessWidget {
           onFieldSubmitted: (v) {
             FocusScope.of(context).requestFocus(nextFocusField);
           },
+          validator: validation,
           style: Theme.of(context).textTheme.title,
         ),
         const SizedBox(height: 8.0),
